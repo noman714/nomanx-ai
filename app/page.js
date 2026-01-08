15bef5ff24d94a5c09bad0cf0c35a3980401fc57
noman714 <nomanali2764@gmail.com>
2026-01-08 23:08:23 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,3 +3,8 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return <Hero />;
     }
+import ChatBox from "./ChatBox";
+
+export default function ChatPage() {
+  return <ChatBox />;
+    }
