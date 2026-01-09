5c73e9939ac0b8801b2f5a49618a14f0464bbe88
noman714 <nomanali2764@gmail.com>
2026-01-09 15:27:00 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,3 +8,6 @@ import ChatBox from "./ChatBox";
 export default function ChatPage() {
   return <ChatBox />;
     }
+if (user.used_tokens > 1000 && user.plan === "free") {
+  return Response.json({ error: "Limit reached" });
+}
